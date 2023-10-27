db0bfbba1802bae1cfe52174004876e266aac622
Fernando Porto <feporto.a@gmail.com>
2023-10-27 08:44:53 -0300

    First Commit - Uploading Springboot files

---
+++ src/test/java/br/edu/fmpsc/GerenciamentoIlab/GerenciamentoIlabApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.fmpsc.GerenciamentoIlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoIlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
