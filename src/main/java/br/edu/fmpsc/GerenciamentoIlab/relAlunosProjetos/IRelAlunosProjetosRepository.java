fbb5316ae44b0a16602c365d7c4be108850b72ba
Fernando Gabriel Porto de Azevedo <feporto.a@gmail.com>
2023-11-30 01:41:02 -0300

    Resolvido problema com Rel Resolvido linhas 31 a 36 do RelAlunosProjetosController: Rel agora nao tem problemas com ids de um aluno ou projeto anteriormente cadastrado

--- src/main/java/br/edu/fmpsc/GerenciamentoIlab/relAlunosProjetos/IRelAlunosProjetosRepository.java
+++ src/main/java/br/edu/fmpsc/GerenciamentoIlab/relAlunosProjetos/IRelAlunosProjetosRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface IRelAlunosProjetosRepository extends JpaRepository<RelAlunosProjetos, UUID>{
     RelAlunosProjetos findByProjetoId(UUID projeto_id);
     RelAlunosProjetos findByAlunoId(UUID aluno_id);
+    Object findByProjetoIdAndAlunoId(UUID projetoId, UUID alunoId);
 }
--- src/main/java/br/edu/fmpsc/GerenciamentoIlab/relAlunosProjetos/RelAlunosProjetosController.java
+++ src/main/java/br/edu/fmpsc/GerenciamentoIlab/relAlunosProjetos/RelAlunosProjetosController.java
@@ -1,16 +1,22 @@
 package br.edu.fmpsc.GerenciamentoIlab.relAlunosProjetos;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.fmpsc.GerenciamentoIlab.projetos.Projeto;
+import br.edu.fmpsc.GerenciamentoIlab.projetos.ProjetoController;
 
 @RestController
 @RequestMapping("/alunos/projetos")
@@ -19,16 +25,16 @@ public class RelAlunosProjetosController {
     @Autowired
     private IRelAlunosProjetosRepository relRepository;
 
+    private ProjetoController projetoController;
+
     @PostMapping("")
     public ResponseEntity createRel(@RequestBody RelAlunosProjetos rel)
     {
-        var projetoId = this.relRepository.findByProjetoId(rel.getProjetoId());
-        var alunoId = this.relRepository.findByAlunoId(rel.getAlunoId());
-
-        if(projetoId != null && alunoId != null){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Projeto e Aluno ja estao relacionados");
+        var existingRel = this.relRepository.findByProjetoIdAndAlunoId(rel.getProjetoId(), rel.getAlunoId());
+        if(existingRel != null){
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Projeto e Aluno ja estao relacionados");
         }
-
+        
         var relCreated = this.relRepository.save(rel);
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(relCreated);
     }
@@ -58,20 +64,22 @@ public class RelAlunosProjetosController {
     // Agora eh pra ser usado no AlunosController
     public ResponseEntity createRel(List<UUID> projetos_id, UUID aluno_id)
     {
-        int totalAlunos = projetos_id.size();
-        int alunosCarregados = 0; // Começa como 0 pq null ia dar problema
-        List<RelAlunosProjetos> relList;
+        if(projetos_id != null && projetos_id.size() > 0){
+            int totalAlunos = projetos_id.size();
+            int alunosCarregados = 0; // Começa como 0 pq null ia dar problema
+            List<RelAlunosProjetos> relList;
+            
+            for(UUID p : projetos_id){
+                relList = this.list(); // Com certeza nao eh a melhor opcao, assim como eu nao sou o melhor programador do mundo
 
-        for(UUID p : projetos_id){
-            relList = this.list(); // Com certeza nao eh a melhor opcao, assim como eu nao sou o melhor programador do mundo
-
-            if(relList.indexOf(new RelAlunosProjetos(p, aluno_id)) == -1){ // Se nao tiver nem o projeto e nem o aluno atual, salva no banco
-                RelAlunosProjetos rel = new RelAlunosProjetos(p, aluno_id); // Como o RelAlunosProjeto nao foi passado nos parametros, passamos ele por aqui
-                this.relRepository.save(rel);
+                if(relList.indexOf(new RelAlunosProjetos(p, aluno_id)) == -1){ // Se nao tiver nem o projeto e nem o aluno atual, salva no banco
+                    RelAlunosProjetos rel = new RelAlunosProjetos(p, aluno_id); // Como o RelAlunosProjeto nao foi passado nos parametros, passamos ele por aqui
+                    this.relRepository.save(rel);
+                }
             }
-        }
-        if(alunosCarregados > 0){
-            return ResponseEntity.status(HttpStatus.CREATED).body(alunosCarregados + "/" + totalAlunos + " carregados no banco com sucesso.");
+            if(alunosCarregados > 0){
+                return ResponseEntity.status(HttpStatus.CREATED).body(alunosCarregados + "/" + totalAlunos + " carregados no banco com sucesso.");
+            }
         }
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Nenhum aluno carregado no banco de dados");
     }
@@ -80,4 +88,15 @@ public class RelAlunosProjetosController {
     public List<RelAlunosProjetos> list(){
         return relRepository.findAll();
     }
+
+    @GetMapping("/projetos")
+    public List<UUID> listProjetosInAlunos(UUID aluno_id) {
+        List<RelAlunosProjetos> listRel = relRepository.findByAlunoId(aluno_id);
+        List<UUID> projetosId = new ArrayList<>();
+        for(int i = 0; i < listRel.size(); i++){
+            projetosId.add(listRel.get(i).getProjetoId());
+        }
+        return projetosId;
+    }
+
 }
